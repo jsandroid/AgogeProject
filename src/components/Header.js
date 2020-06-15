79b043d5d2c963d5a1260eaf33af8d4399e5bd4a
Jonathan S <jonathan@Jonathans-Air.fios-router.home>
2020-06-14 22:42:32 -0400

    fixed header bug

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,23 +3,23 @@ import { Link } from 'gatsby'
 import logo from  '../images/agoge_white.png'
 import headerStyles from './header.module.css'
 
-function Navigation({subStyles, subMenuBehavior}) {
+function Navigation() {
   return(
     <nav>
       <ul>
-        <li onMouseEnter={() => subMenuBehavior(null)}><Link className={headerStyles.navLink} to='/'>About</Link></li>
-        <li onMouseEnter={() => subMenuBehavior(null)}><Link className={headerStyles.navLink} to='/'>Values</Link></li>
-        <li onMouseEnter={() => subMenuBehavior(null)}><Link className={headerStyles.navLink} to='/'>Team</Link></li>
-        <li onMouseEnter={() => subMenuBehavior(null)}><Link className={headerStyles.navLink} to='/'>Programs</Link></li>
-        <li onMouseEnter={() => subMenuBehavior('News')}>
+        <li><Link className={headerStyles.navLink} to='/'>About</Link></li>
+        <li><Link className={headerStyles.navLink} to='/'>Values</Link></li>
+        <li><Link className={headerStyles.navLink} to='/'>Team</Link></li>
+        <li><Link className={headerStyles.navLink} to='/'>Programs</Link></li>
+        <li>
           <Link className={headerStyles.navLink} to='/'>News</Link>
-          <ul className={subStyles.News} onMouseLeave={() => subMenuBehavior(null)}>
+          <ul>
             <li><Link to='/'>Student Profiles</Link></li>
           </ul>
         </li>
-        <li onMouseEnter={() => subMenuBehavior('Involvement')}>
+        <li>
           <Link className={headerStyles.navLink} to='/'>Get Involved</Link>
-          <ul className={subStyles.Involvement} onMouseLeave={() => subMenuBehavior(null)}>
+          <ul>
             <li><Link to='/'>Mentoring</Link></li>
             <li><Link to='/'>Coaching</Link></li>
             <li><Link to='/'>Donate</Link></li>
@@ -30,68 +30,25 @@ function Navigation({subStyles, subMenuBehavior}) {
   )
 }
 
-export default class Header extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      activeSubMenu: null,
-      submenuStyles : {
-        News: headerStyles.hide,
-        Involvement: headerStyles.hide
-      }
-    }
-    this.toggleSubMenu = this.toggleSubMenu.bind(this)
-    
-  }
-  
-  toggleSubMenu(subMenu) {
-    if(subMenu === 'Involvement') {
-      this.setState({
-        activeSubMenu: subMenu,
-        submenuStyles : {
-          News: headerStyles.hide,
-          Involvement: headerStyles.show
-        }
-      })
-    } else if(subMenu === 'News') {
-      this.setState({
-        activeSubMenu: subMenu,
-        submenuStyles : {
-          News: headerStyles.show,
-          Involvement: headerStyles.hide
-        }
-      })
-    } else {
-      this.setState({
-        activeSubMenu: null,
-        submenuStyles : {
-          News: headerStyles.hide,
-          Involvement: headerStyles.hide
-        }
-      })
-    }
-  }
-
-
-  render() {
-    return(
-      <header>
-        <div className={headerStyles.globalHeader}>
-          <div className={headerStyles.spacebetween}>
-            <div id='logo' className={headerStyles.logo}>
-              <Link to='/' className={headerStyles.logoLink}>
-                <img src={logo} alt='Agoge Project Logo'/>
-                <span>Agoge Project</span>
-              </Link>
-            </div>
-              <Navigation subStyles={this.state.submenuStyles} subMenuBehavior={this.toggleSubMenu}/>
-            <div className={headerStyles.nav_donate}>
-              <Link className={headerStyles.donateBtn} to='/'>Donate</Link>
-            </div>
-            
+export default function Header() {
+  return(
+    <header>
+      <div className={headerStyles.globalHeader}>
+        <div className={headerStyles.spacebetween}>
+          <div id='logo' className={headerStyles.logo}>
+            <Link to='/' className={headerStyles.logoLink}>
+              <img src={logo} alt='Agoge Project Logo'/>
+              <span>Agoge Project</span>
+            </Link>
           </div>
+            <Navigation />
+          <div className={headerStyles.nav_donate}>
+            <Link className={headerStyles.donateBtn} to='/'>Donate</Link>
+          </div>
+          
         </div>
-      </header>
-    )
-  }
+      </div>
+    </header>
+  )
+
 }
