801db4a082d1785635dc00cd159fdc8781fae4b8
Jonathan Sanchez <jsandroid@gmail.com>
2020-08-17 20:56:34 -0400

    firebase and GCP

--- src/pages/submitted.js
+++ src/pages/submitted.js
@@ -4,14 +4,15 @@ import PropTypes from 'prop-types'
 import { FaEnvelopeOpenText } from 'react-icons/fa'
 
 export default function Submitted({ location }) {
-  console.log(location)
+  var name = ''
+  if(typeof window !== 'undefined' && location.state !== null) name = location.state.name.trim()
   return(
     <Layout>
       <div className='content'>
         <div className='contactSubmission'>
           <FaEnvelopeOpenText />
           <h1>Contact Form Submitted!</h1>
-          <p>Thanks {location.state.name} for reaching out! We'll be in contact within a few days.</p>
+          <p>Thanks {name} for reaching out! We'll be in contact within a few days.</p>
         </div>
       </div>
     </Layout>
