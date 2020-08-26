bf89f4e472c4db8169555eea459e5641854c195a
Jonathan Sanchez <jsandroid@gmail.com>
2020-08-26 13:01:42 -0400

    #12 updated mobile navigation

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,51 +2,141 @@ import React from 'react'
 import { Link } from 'gatsby'
 import logo from  '../images/agoge_white.png'
 import headerStyles from './header.module.css'
+import { FaBars } from 'react-icons/fa'
 
-function Navigation() {
+const menuLinks = [
+  {
+    name: 'About',
+    link: '/about',
+    sublinks: [
+      {
+        name: 'Our Team',
+        link: '/team',
+        sublinks: []
+      },
+      {
+        name: 'Our Board',
+        link: '/board',
+        sublinks: []
+      },
+      {
+        name: 'Values',
+        link: '/values',
+        sublinks: []
+      },
+    ]
+  },
+  {
+    name: 'Programs',
+    link: '/programs',
+    sublinks: []
+  },
+  {
+    name: 'News',
+    link: '/news',
+    sublinks: []
+  },
+  {
+    name: 'Get Involved',
+    link: '/involvement',
+    sublinks: []
+  },
+  {
+    name: 'Contact Us',
+    link: '/contact',
+    sublinks: []
+  }
+]
+
+function Navigation({ burgerClick }) {
   return(
-    <nav>
+    <React.Fragment>
+      <nav className={headerStyles.desktopNav}>
+        <ul>
+          { menuLinks.map((navLink, index) => {
+            return !navLink.sublinks.length 
+            ? <li><Link tabIndex={index + 1} className={headerStyles.navLink} to={navLink.link}>{navLink.name}</Link></li>
+            : <li><Link tabIndex={index + 1} className={headerStyles.navLink} to={navLink.link}>{navLink.name}</Link>
+              <ul>
+              {navLink.sublinks.map((subLink) => {
+                return( <li><Link to={subLink.link}>{subLink.name}</Link></li>)
+              })}
+            </ul></li> 
+          })
+          }
+        </ul>
+      </nav>
+      <div className={headerStyles.nav_donate}>
+        <a tabIndex='7' className={headerStyles.donateBtn} href='https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=KPCSJHZA6URES&source=url'>Donate</a>
+      </div>
+      <div className={headerStyles.burgerMenu}>
+        <FaBars onClick={burgerClick} />
+      </div>
+    </React.Fragment>
+  )
+}
+
+function MobileNav({ mobileMenuStyle }) {
+  return(
+    <nav className={`${mobileMenuStyle} ${headerStyles.mobileNav}`}>
       <ul>
-        <li><Link tabIndex='2' className={headerStyles.navLink} to='/about'>About</Link>
-          <ul>
-            <li><Link to='/team'>Our Team</Link></li>
-            <li><Link to='/board'>Our Board</Link></li>
-            <li><Link to='/values'>Values</Link></li>
+        { menuLinks.map((navLink, index) => {
+          return !navLink.sublinks.length 
+          ? <li><Link tabIndex={index + 1} className={headerStyles.mobileNavLink} to={navLink.link}>{navLink.name}</Link></li>
+          : <div><li><Link tabIndex={index + 1} className={headerStyles.mobileNavLink} to={navLink.link}>{navLink.name}</Link></li>
+            <ul>
+            {navLink.sublinks.map((subLink) => {
+              return( <li><Link to={subLink.link}>{subLink.name}</Link></li>)
+            })}
           </ul>
-        </li>
-        <li><Link tabIndex='3' className={headerStyles.navLink} to='/programs'>Programs</Link></li>
-        <li>
-          <Link tabIndex='4' className={headerStyles.navLink} to='/news'>News</Link>
-          {/*<ul>
-            <li><Link to='/students'>Student Profiles</Link></li>
-          </ul>*/}
-        </li>
-        <li><Link tabIndex='5' className={headerStyles.navLink} to='/involvement'>Get Involved</Link></li>
-        <li><Link tabIndex='6' className={headerStyles.navLink} to='/contact'>Contact Us</Link></li>
+          </div>
+        })
+        }
       </ul>
     </nav>
   )
 }
 
-export default function Header() {
-  return(
-    <header>
-      <div className={headerStyles.globalHeader}>
-        <div className={headerStyles.spacebetween}>
-          <div id='logo' className={headerStyles.logo}>
-            <Link tabIndex='1' to='/' className={headerStyles.logoLink}>
-              <img src={logo} alt='Agoge Project Logo'/>
-              <span>The Agoge Project</span>
-            </Link>
+export default class Header extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      navbarOpen: false,
+      mobileMenuCss: headerStyles.hideMobileMenu
+    }
+
+    this.toggleMobileMenu = this.toggleMobileMenu.bind(this)
+  }
+
+  toggleMobileMenu () {
+    this.state.navbarOpen
+      ? this.setState({
+        navbarOpen: false,
+        mobileMenuCss: headerStyles.hideMobileMenu
+      })
+      : this.setState({
+        navbarOpen: true,
+        mobileMenuCss: headerStyles.showMobileMenu
+      })
+  }
+
+  render() {
+    return(
+      <header>
+        <div className={headerStyles.globalHeader}>
+          <div className={headerStyles.spacebetween}>
+            <div id='logo' className={headerStyles.logo}>
+              <Link tabIndex='0' to='/' className={headerStyles.logoLink}>
+                <img src={logo} alt='Agoge Project Logo'/>
+                <span>The Agoge Project</span>
+              </Link>
+            </div>
+            <Navigation burgerClick = {this.toggleMobileMenu} />     
           </div>
-            <Navigation />
-          <div className={headerStyles.nav_donate}>
-            <a tabIndex='7' className={headerStyles.donateBtn} href='https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=KPCSJHZA6URES&source=url'>Donate</a>
-          </div>
-          
         </div>
-      </div>
-    </header>
-  )
+        <MobileNav mobileMenuStyle = {this.state.mobileMenuCss} />
+      </header>
+    )
+  }
 
 }
